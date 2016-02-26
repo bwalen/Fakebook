7572216ee5ba07cdc24bbe9cf590f826418209bc
Brian Walen <bwalen@hotmail.com>
2016-02-26 15:51:30 -0800

    I continued to work on formatting.  Added dummy content to the sidebars in preparation for dynamic content.

---
+++ project-one/data.js
@@ -0,0 +1,14 @@
+var user = {
+  firstName: John,
+  lastName: Doe,
+  profilePicture: imgurl,
+  isMyFriend: True
+
+}
+
+var timelinePost{
+  user: john,
+  date: 20160225,
+  text: "Blah blah blah",
+  picture: imgurl
+}
