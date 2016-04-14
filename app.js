907cb43cb09fa530449e3768b0a5c762d0305c09
Brian Walen <bwalen@hotmail.com>
2016-04-14 11:01:29 -0700

    updated webserver.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.use(express.static("./"));
 
 app.listen(8080, function(){
-  console.log("Listening on port 8080");
+  console.log("Listening on port " + port);
 });
